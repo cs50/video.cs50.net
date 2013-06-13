33e592ac2e0144001c9368f7d4b7907914d8228b
Joseph Ong <ong.joseph.y@gmail.com>
2013-06-13 17:43:12 -0400

    made some controls work, closed captioning

--- html/lib/video50.js
+++ html/lib/video50.js
@@ -36,7 +36,7 @@ CS50.Video = function(options) {
         defaultLanguage: 'en',
         onReady: false,
         numVideos: 1,
-        playbackRates: [0.75, 1, 1.25, 1.5],
+        playbackRates: [0.7, 1, 1.2, 1.5],
         title: '',
     }, this.options);
 
@@ -49,6 +49,9 @@ CS50.Video = function(options) {
                 <div class="left"> \
                   <div class="main-video-wrapper"> \
                     <canvas class="video-canvas" width="640" height="360" data-segment="1"></canvas> \
+                    <div class="video50-cc-container"> \
+                        <div class="video50-cc-text"></div> \
+                    </div> \
                   </div> \
                 </div> \
                 <% if (numVideos > 1) { %> \
@@ -72,18 +75,21 @@ CS50.Video = function(options) {
                 </div> \
               </div> \
               <div class="time"> \
-                <div class="timecode">00:55</div> \
-                <div class="progress"></div> \
-                <div class="timeline"></div> \
-                <div class="timelength">13:37</div> \
+                <div class="timecode"></div> \
+                <div class="timeline"><div class="progress"></div></div> \
+                <div class="timelength"></div> \
               </div> \
               <div class="right-controls"> \
                 <div class="download-control"></div><div class="captions-control"> \
+                    <div class="video50-transcript-container"></div> \
+                </div><div class="speed-control" data-speed="1"> \
+                    1x \
                 </div><div class="quality-control"> \
                   HD \
                 </div><div class="fullscreen-control"> \
                 </div> \
               </div> \
+            </div> \
         ',
     };
 
@@ -100,6 +106,8 @@ CS50.Video = function(options) {
  *  Creates a new instance of the player in the specified container.
  */
 CS50.Video.prototype.createPlayer = function() {
+    var me = this;
+    
     // clear out old containers and add video player html
     var $container = $(this.options.playerContainer);
     $container.empty();
@@ -204,26 +212,181 @@ CS50.Video.prototype.createPlayer = function() {
         }
     });
 
+    // video related updates 
+    var video = $container.find(".source-video")[0];
+
+    // play pause
+    $container.on('mousedown', '.play-pause-control', function() {
+        $(this).toggleClass('pause');
+    
+        if ($(this).hasClass('pause')) {
+            video.pause();
+        } else {
+            video.play();
+        }
+    });
+ 
+    // seek around by clicking on the progress bar
+    $container.on('mousedown', '.timeline', function(e) {
+        var ratio = (e.pageX - $(this).offset().left)/$(this).width();
+        video.currentTime = ratio * video.duration;
+    });    
+
+    // skip back 30 seconds on the 30 second button
+    $container.on('mousedown', '.skip-control', function(e) {
+        var time = video.currentTime < 30 ? 0 : video.currentTime - 30;
+        video.currentTime = time;
+    });    
+
+    // toggle video speed
+    $container.on('mousedown', '.speed-control', function(e) {
+        var index = (parseInt($(this).attr('data-speed')) + 1) % me.options.playbackRates.length;
+        var speed = me.options.playbackRates[index];
+        $(this).attr('data-speed', index);
+        $(this).text(speed + "x");
+        video.playbackRate = speed;
+     });    
+
     // trigger a resize to get the correct dimensions for the initialization
     $(window).trigger('resize');
 
+    // load closed captioning and set closed captioning to happen in a loop
+    this.loadSrt(this.options.defaultLanguage);
+    $container.find('.source-video').on('timeupdate', function(e) {
+        if (!this.lastUpdate || (this.lastUpdate + 500) < (new Date).getTime()) {
+            // update highlight on the transcript, update cc
+            me.updateTranscriptHighlight(e.target.currentTime + 7);
+            me.updateCC(e.target.currentTime + 7);
+            me.updateTimeline(e.target.currentTime);           
+ 
+            this.lastUpdate = (new Date).getTime();
+        }
+    });
+
     // set redrawing the video to run on a loop
-    var video = $(".source-video")[0];
     this.redrawVideo(video);
+};
+
+CS50.Video.prototype.updateTimeline = function(time) {
+    var time = Math.floor(time);
+    var $container = $(this.options.playerContainer);
+    var video = $container.find('.source-video')[0];   
+ 
+    // update the length
+    if ($container.find('.timelength').text() == "") {
+        var total = Math.floor(video.duration);
+        var m = Math.floor(total / 60);
+        var s = total % 60;
+        m = m < 10 ? "0" + m : m;
+        s = s < 10 ? "0" + s : s;
+        $container.find('.timelength').text(m + ":" + s);   
+    }
+
+    // update the timecode
+    var m = Math.floor(time / 60);
+    var s = time % 60;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+    $container.find('.timecode').text(m + ":" + s);
+
+    // update the length of the bar
+    var ratio = time/video.duration;
+    $container.find('.progress').css("width", (ratio * 100) + "%");
 };
 
 /*
  *  Updates the closed captioning for the video player.
  */
-CS50.Video.prototype.updateCC = function() {
+CS50.Video.prototype.updateCC = function(time) {
+    var time = Math.floor(time);
+    var $container = $(this.options.playerContainer);
+    var $active = $container.find('.video50-transcript-container [data-time="' + time + '"]');
+    var $text = $container.find('.video50-cc-text');
+
+    // if current CC is not correct
+    if ($active.length && $text.attr('data-time') != time) {
+        $text.text($active.text());
+    }  
 };
 
-/*
- *  Updates the transcript highlight for the video player.
+/**
+ * Highlight the line corresponding to the current point in the video in the transcript
  *
  */
-CS50.Video.prototype.updateTranscript = function() {
+CS50.Video.prototype.updateTranscriptHighlight = function(time) {
+    var time = Math.floor(time);
+    var $container = $(this.options.playerContainer).find('.video50-transcript-container');
+    var $active = $container.find('[data-time="' + time + '"]');
+
+    // check if a new element should be highlighted
+    if ($active && $active.length) {
+        // remove all other highlights
+        $container.find('a').removeClass('highlight');
+
+        // add highlight to active element
+        $active.addClass('highlight');
+    }
 };
+
+/**
+ * Load the specified SRT file.
+ *
+ * @param lang Language to load
+ *
+ */
+CS50.Video.prototype.loadSrt = function(language) {
+    this.srtData = {};
+    var player = this.player;
+    var me = this;
+
+    if (this.options.srt[language]) {
+        $.get(this.options.srt[language], function(response) {
+            var timecodes = response.split(/\n\s*\n/);
+
+            // if transcript container is given, then build transcript
+            if (_.keys(me.options.srt).length) {
+                // clear previous text
+                var $container = $(me.options.playerContainer).find('.video50-transcript-container');
+                
+                // look for a previous caption if already active, keep the timecode
+                var oldTime = $container.find('.highlight[data-time]').attr('data-time');
+                $container.empty();
+
+                // iterate over each timecode
+                var n = timecodes.length;
+                for (var i = 0; i < n; i++) {
+                    // split the elements of the timecode
+                    var timecode = timecodes[i].split("\n");
+                    if (timecode.length > 1) {
+                        // extract time and content from timecode
+                        var timestamp = timecode[1].split(" --> ")[0];
+                        timecode.splice(0, 2);
+                        var content = timecode.join(" ");
+
+                        // if line starts with >> or [, then start a new line
+                        if (content.match(/^(>>|\[)/))
+                            $container.append('<br /><br />');
+
+                        // convert from hours:minutes:seconds to seconds
+                        var time = timestamp.match(/(\d+):(\d+):(\d+)/);
+                        var seconds = parseInt(time[1], 10) * 3600 + parseInt(time[2], 10) * 60 + parseInt(time[3], 10);
+
+                        // add line to transcript
+                        $container.append('<a href="#" data-time="' + seconds + '">' + content + '</a> ');
+                    }
+                }
+
+                // if there was a previously active timecode, update cc and highlight
+                if (oldTime) {
+                    var time = { position: oldTime };
+                    me.updateCC(time);
+                    me.updateTranscriptHighlight(time);
+                }
+            }
+        });
+    }
+};
+
 
 /*
  *  Draws a frame from the video to the canvases.
