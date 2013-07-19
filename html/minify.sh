b81c91cae3ddc02baec127e640bca234f8cc131e
Joseph Ong <ong.joseph.y@gmail.com>
2013-07-18 22:38:19 -0400

    minification working

--- html/minify.sh
+++ html/minify.sh
@@ -1,30 +1,31 @@
 #!/bin/sh
 
-rm -rf build
-mkdir -p build/img
-mkdir -p build/js
-mkdir -p build/css
-mkdir -p build/examples
+rm -rf video50/build
+mkdir -p video50/build/img
+mkdir -p video50/build/js
+mkdir -p video50/build/css
 
 echo "Building JavaScript..."
 cat \
-    video50/lib/underscore-min.js \
-    video50/lib/jwplayer/jwplayer.js \
-    video50/js/video50.js \
-    video50/js/video50.languages.js \
-    > build/js/cs50.video.min.js
+    video50/src/lib/underscore-min.js \
+    video50/src/lib/jwplayer/jwplayer.js \
+    video50/src/js/video50.js \
+    video50/src/js/video50.languages.js \
+    > video50/build/js/cs50.video.min.js
 
-java -jar yuicompressor-2.4.7.jar -o build/js/cs50.video.min.js build/js/cs50.video.min.js
+java -jar yuicompressor-2.4.7.jar -o video50/build/js/cs50.video.min.js video50/build/js/cs50.video.min.js
 
-sass video50/css/video50.scss > video50/css/video50.css
+sass video50/src/css/video50.scss > video50/src/css/video50.css
 
 echo "Building CSS..."
 cat \
-    video50/css/video50.css \
-    > build/css/cs50.video.min.css
-java -jar yuicompressor-2.4.7.jar -o build/css/cs50.video.min.css build/css/cs50.video.min.css
+    video50/src/css/video50.css \
+    > video50/build/css/cs50.video.min.css
+java -jar yuicompressor-2.4.7.jar -o video50/build/css/cs50.video.min.css video50/build/css/cs50.video.min.css
 
 echo "Copying assets..."
-cp video50/lib/jwplayer/jwplayer.flash.swf build/jwplayer.flash.swf
-cp video50/img/* build/img
-cp video50/examples/* build/examples
+cp video50/src/lib/jwplayer/jwplayer.flash.swf video50/build/jwplayer.flash.swf
+cp video50/src/img/* video50/build/img
+cp -r video50/src/examples video50/build/
+mv -f video50/build/examples/1-build.html video50/build/examples/1.html
+mv -f video50/build/examples/2-build.html video50/build/examples/2.html
