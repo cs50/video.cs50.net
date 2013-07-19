7a31927234dfb06ea09a831a04412bc49790c576
David J. Malan <malan@harvard.edu>
2013-07-19 00:55:49 -0400

    tidied scripts

---
+++ bin/minify.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# http://stackoverflow.com/a/4774063
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd`
+popd > /dev/null
+
+rm -rf $SCRIPTPATH/../html/video50/build
+mkdir -p $SCRIPTPATH/../html/video50/build/{css,img,js}
+
+echo "Building JavaScript..."
+cat \
+    $SCRIPTPATH/../html/video50/src/lib/underscore-min.js \
+    $SCRIPTPATH/../html/video50/src/lib/jwplayer/jwplayer.js \
+    $SCRIPTPATH/../html/video50/src/js/video50.js \
+    $SCRIPTPATH/../html/video50/src/js/video50.languages.js \
+    > $SCRIPTPATH/../html/video50/build/js/cs50.video.min.js
+
+java -jar $SCRIPTPATH/../lib/yuicompressor-2.4.7.jar -o $SCRIPTPATH/../html/video50/build/js/cs50.video.min.js $SCRIPTPATH/../html/video50/build/js/cs50.video.min.js
+
+sass $SCRIPTPATH/../html/video50/src/css/video50.scss > $SCRIPTPATH/../html/video50/src/css/video50.css
+
+echo "Building CSS..."
+cat \
+    $SCRIPTPATH/../html/video50/src/css/video50.css \
+    > $SCRIPTPATH/../html/video50/build/css/cs50.video.min.css
+java -jar $SCRIPTPATH/../lib/yuicompressor-2.4.7.jar -o $SCRIPTPATH/../html/video50/build/css/cs50.video.min.css $SCRIPTPATH/../html/video50/build/css/cs50.video.min.css
+
+echo "Copying assets..."
+cp $SCRIPTPATH/../html/video50/src/lib/jwplayer/jwplayer.flash.swf $SCRIPTPATH/../html/video50/build/jwplayer.flash.swf
+cp $SCRIPTPATH/../html/video50/src/img/* $SCRIPTPATH/../html/video50/build/img
+cp -r $SCRIPTPATH/../html/video50/src/examples $SCRIPTPATH/../html/video50/build/
+mv -f $SCRIPTPATH/../html/video50/build/examples/1-build.html $SCRIPTPATH/../html/video50/build/examples/1.html
+mv -f $SCRIPTPATH/../html/video50/build/examples/2-build.html $SCRIPTPATH/../html/video50/build/examples/2.html
