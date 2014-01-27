0d5913279e8b22ef87f0c79322bf9977b8e1a02d
David J. Malan <malan@harvard.edu>
2014-01-26 19:47:57 -0500

    Added CS161 mixpanel token

--- html/161/index.php
+++ html/161/index.php
@@ -27,7 +27,7 @@
         <script>
 
             $(function() {
-                new CS50.Video("#v", {"captions":[],"downloads":[{"label":"MP4","src":<?= json_encode($_GET["src"]) ?>}],"sources":{"source":[{"src":<?= json_encode($_GET["src"]) ?>,"type":"video\/mp4"}]}});
+                new CS50.Video("#v", {"analytics50": {"mixpanel": {"token": "56c3b1504dd27b8f0c82b72d653b7c94"}}, "captions":[],"downloads":[{"label":"MP4","src":<?= json_encode($_GET["src"]) ?>}],"sources":{"source":[{"src":<?= json_encode($_GET["src"]) ?>,"type":"video\/mp4"}]}});
             });
 
         </script>
