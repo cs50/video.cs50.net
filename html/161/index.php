aa6fc8750d57713ecbc10a05e0b49c7402da4d9e
Rob Bowden <rob@cs.harvard.edu>
2014-01-29 12:08:54 -0500

    add identity to analytics

--- html/161/index.php
+++ html/161/index.php
@@ -3,15 +3,27 @@
     // configuration
     require_once(dirname(__FILE__) . "/config.php");
 
+    if (isset($_GET["src"]))
+    {
+    	if (filter_var($_GET["src"], FILTER_VALIDATE_URL) === false) {
+	    http_response_code(404);
+	    die("404 Not Found");
+	}
+	else {
+	    $_SESSION['src'] = $_GET['src'];
+	}
+    }
+
+    if (!isset($_SESSION['src'])) {
+	http_response_code(404);
+	die("404 Not Found");
+    }
+
     if (!isset($_SESSION['user'])) {
     	header('Location: https://video.cs50.net/161/login.php');
         exit;
     }
 
-    if (!isset($_GET["src"]) || filter_var($_GET["src"], FILTER_VALIDATE_URL) === false) {
-        http_response_code(404);
-        die("404 Not Found");
-    }
 
 ?>
 
@@ -39,8 +51,11 @@
 		    "#v",
 		    {"analytics50": {"mixpanel": {"token": "56c3b1504dd27b8f0c82b72d653b7c94"}},
 		    "captions":[],"downloads":[{"label":"MP4","src":<?= json_encode($_GET["src"])
-		    ?>}],"sources":{"source":[{"src":<?= json_encode($_GET["src"]) ?>,"type":"video\/mp4"}]}},
-		    
+		    ?>}],"sources":{"source":[{"src":<?= json_encode($_SESSION["src"]) ?>,"type":"video\/mp4"}]}},
+		    {
+		    	identify: "<?= $_SESSION['user']['email'] ?>",
+		    	nametag: "<?= $_SESSION['user']['fullname'] ?>"
+		    }
 		);
             });
 
