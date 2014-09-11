3e7bda7c9b2a68355d14ae52aa071409b99ca3b5
David J. Malan <malan@harvard.edu>
2014-09-11 18:54:29 -0400

    Tidied repo.

--- public/scss.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    // sass
-    header("Content-Type: text/css");
-    if (file_exists(__DIR__ . $_SERVER["SCRIPT_URL"])) {
-        print(shell_exec("/usr/local/bin/sass --no-cache " . escapeshellarg(__DIR__ . $_SERVER["SCRIPT_URL"])));
-    }
-
-?>
