13fe8e6a04b5f70027eeacd4c1fd9b0c8853e227
Luke Jackson <lukejacksonn@gmail.com>
2016-07-22 16:30:49 -0400

    Abstracted video-controls from search module and reimplemented download

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import Timeline from './modules/timeline';
 import Search from './modules/search';
 import Video from './modules/video';
 import Playback from './modules/playback';
+import Control from './modules/control';
+import Download from './modules/download';
 
 /*
 {
@@ -23,6 +25,11 @@ export default () => {
   // Initialize chapter-list with the chapters JSON file
   Chapter.init('chapter-list', '/chapters/output.json');
   // Initialize chapter-timeline with the chapters JSON file
+  Control.init('video-controls', [{}]);
+  Download.init('video-download', [{
+    name: 'S15E01',
+    href: 'http://cdn.cs50.net/2015/fall/lectures/0/w/week0w-720p.mp4',
+  }]);
   Timeline.init('chapter-timeline', '/chapters/output.json');
   // Initialize chapter-search with target chapter-list
   Search.init('chapter-search', [{ target: 'chapter-list' }]);
@@ -31,4 +38,14 @@ export default () => {
   Video.init('video-support', { vid: 'BRpel-vjckA', controls: 0, mute: true });
   // Initialize playback speed widget with no options
   Playback.init('chapter-playback', [{}]);
+
+  const $aside = document.querySelector('aside');
+  let hider;
+  document.addEventListener('mousemove', () => {
+    if (hider !== null) clearTimeout(hider);
+    $aside.classList.remove('hidden');
+    hider = setTimeout(() => {
+      $aside.classList.add('hidden');
+    }, 3000);
+  });
 };
--- src/modules/video/index.js
+++ src/modules/video/index.js
@@ -63,62 +63,63 @@ export default {
       if (state === 2) player.pauseVideo();
     });
 
-    // Fetch video description (chapters)
-    fetch(`https://www.googleapis.com/youtube/v3/videos?id=${ops.vid}&key=AIzaSyBJB0_EuZdaddGDTyOC1oO3CXCRq9_EAiA&part=snippet`)
-    .then(data => data.json())
-    .then(json => json.items[0].snippet.description)
-    .then(desc => `
-${desc}
-
-# Chapter Index
-
-00:00 | Intro
-00:10 | Teaser Video
-02:33 | Welcome to CS50
-07:01 | Binary
-09:38 | Binary Bulbs
-16:24 | Searching (Tearing) a Phone Book
-20:36 | Computational Thinking
-23:35 | Pseudocode
-26:40 | Mark Zuckerberg's CS50 Lecture
-29:36 | Staff Introductions
-34:56 | Course Overview
-42:00 | Selfie Stick Introductions
-44:52 | Outro`)
-    .then(desc => {
-      const chapters = desc.split('\n')
-      .filter(x => x.match(/\d+:\d+\s\|\s/))
-      .map((x, index, arr) => ({
-        id: index,
-        start: x.split(' | ')[0],
-        end: arr[index + 1] ? arr[index + 1].split(' | ')[0] : null,
-        title: x.split(' | ')[1],
-      }));
-      console.log(chapters);
-    });
-
-    // Fetch video captions
-    fetch(`https://video.google.com/timedtext?lang=en&v=${ops.vid}`)
-    .then(data => data.text())
-    .then(text => xml2js.parseString(text, (err, res) => {
-      const captions = res.transcript.text
-      .filter(x => x._ !== undefined)
-      .map(x => ({
-        start: parseFloat(x.$.start).toFixed(2),
-        dur: parseFloat(x.$.dur).toFixed(2),
-        end: (parseFloat(x.$.start) + parseFloat(x.$.dur)).toFixed(2),
-        caption: x._,
-      }));
-      console.log(captions);
-    }));
+//     // Fetch video description (chapters)
+//     fetch(`https://www.googleapis.com/youtube/v3/videos?id=${ops.vid}&key=AIzaSyBJB0_EuZdaddGDTyOC1oO3CXCRq9_EAiA&part=snippet`)
+//     .then(data => data.json())
+//     .then(json => json.items[0].snippet.description)
+//     .then(desc => `
+// ${desc}
+// 
+// # Chapter Index
+//
+// 00:00 | Intro
+// 00:10 | Teaser Video
+// 02:33 | Welcome to CS50
+// 07:01 | Binary
+// 09:38 | Binary Bulbs
+// 16:24 | Searching (Tearing) a Phone Book
+// 20:36 | Computational Thinking
+// 23:35 | Pseudocode
+// 26:40 | Mark Zuckerberg's CS50 Lecture
+// 29:36 | Staff Introductions
+// 34:56 | Course Overview
+// 42:00 | Selfie Stick Introductions
+// 44:52 | Outro`)
+//     .then(desc => {
+//       const chapters = desc.split('\n')
+//       .filter(x => x.match(/\d+:\d+\s\|\s/))
+//       .map((x, index, arr) => ({
+//         id: index,
+//         start: x.split(' | ')[0],
+//         end: arr[index + 1] ? arr[index + 1].split(' | ')[0] : null,
+//         title: x.split(' | ')[1],
+//       }));
+//       console.log(chapters);
+//     });
+//
+//     // Fetch video captions
+//     fetch(`https://video.google.com/timedtext?lang=en&v=${ops.vid}`)
+//     .then(data => data.text())
+//     .then(text => xml2js.parseString(text, (err, res) => {
+//       const captions = res.transcript.text
+//       .filter(x => x._ !== undefined)
+//       .map(x => ({
+//         start: parseFloat(x.$.start).toFixed(2),
+//         dur: parseFloat(x.$.dur).toFixed(2),
+//         end: (parseFloat(x.$.start) + parseFloat(x.$.dur)).toFixed(2),
+//         caption: x._,
+//       }));
+//       console.log(captions);
+//     }));
 
     subscribe('video:seekTo', time => {
       player.seekTo(time);
       publish('video:tick', [time]);
     });
 
-    subscribe('video:playbackSpeed', time =>
-      player.setPlaybackRate(time));
+    subscribe('video:playbackSpeed', time => player.setPlaybackRate(time));
+    subscribe('video:play', () => player.playVideo());
+    subscribe('video:pause', () => player.pauseVideo());
 
     subscribe('video:seekBy', sec =>
       player.getCurrentTime()
