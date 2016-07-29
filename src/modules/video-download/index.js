a30daec48a8e28f511ca8ce0ac5afbe320ac7031
Luke Jackson <lukejacksonn@gmail.com>
2016-07-29 17:32:03 -0400

    Moved download and playback speek into control bar

--- src/modules/video-download/index.js
+++ src/modules/video-download/index.js
@@ -3,16 +3,18 @@ export default {
     const container = document.querySelector(selector);
     container.innerHTML = '';
     const frag = document.createDocumentFragment();
-    const title = document.createElement('h1');
-    title.innerHTML = '<button><svg viewBox="0 0 1 1"><use xlink:href="#icon-download"></use></svg></button>';
+    const title = document.createElement('button');
+    title.innerHTML = '<svg viewBox="0 0 1 1"><use xlink:href="#icon-download"></use></svg>';
     frag.appendChild(title);
+    const div = document.createElement('div');
     Object.keys(data).forEach(key => {
       const elem = document.createElement('a');
       elem.innerHTML = `${key}p`;
       elem.href = data[key];
       elem.setAttribute('download', 'true');
-      frag.appendChild(elem);
+      div.appendChild(elem);
     });
+    frag.appendChild(div);
     container.appendChild(frag);
   },
 };
--- src/modules/video-playback/index.js
+++ src/modules/video-playback/index.js
@@ -4,10 +4,11 @@ export default {
   render(selector, data) {
     const container = document.querySelector('video-playback');
     const fragment = document.createDocumentFragment();
-    const title = document.createElement('h1');
+    const title = document.createElement('button');
     container.innerHTML = '';
     title.innerHTML = '<svg viewBox="0 0 1 1"><use xlink:href="#icon-speed"></use></svg>';
     fragment.appendChild(title);
+    const div = document.createElement('div');
     data.forEach(rate => {
       const $button = document.createElement('button');
       $button.innerHTML = rate.label;
@@ -20,8 +21,9 @@ export default {
         // Make this rate active
         $button.classList.add('active');
       });
-      fragment.appendChild($button);
+      div.appendChild($button);
     });
+    fragment.appendChild(div);
     container.appendChild(fragment);
   },
 };
