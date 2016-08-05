25f2fe6e403f8e69f1051362ade1e8243ef084c7
Luke Jackson <lukejacksonn@gmail.com>
2016-08-05 12:49:49 -0400

    Removes old modules

--- src/modules/download/template.js
+++
@@ -1,3 +0,0 @@
-export default model =>
-  `<a href='${model.href}' download='${model.name}'>Download</a>
-  <svg viewBox="0 0 1 1"><use xlink:href="#icon-download"></use></svg>`;
--- src/modules/search/template.js
+++
@@ -1,3 +0,0 @@
-export default model =>
-`<input type="text" placeholder="Search Video.." data-target="${model.target}" />
-<svg viewBox="0 0 1 1"><use xlink:href="#icon-search"></use></svg>`;
